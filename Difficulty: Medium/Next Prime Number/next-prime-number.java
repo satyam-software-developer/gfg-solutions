1cd0b4dba576c4d2c0ddb1deb0584881f6892795
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-25 13:12:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Prime Number/next-prime-number.java
@@ -0,0 +1,27 @@
+class Solution {
+	public static int nextPrime(int n) {
+		
+		// code here to find next prime number
+		// return next prime number
+		
+		int num = n + 1;
+		
+		while (true) {
+			boolean prime = true;
+			
+			for (int i = 2; i * i <= num; i++) {
+				if (num % i == 0) {
+					prime = false;
+					break;
+				}
+				
+			}
+			if (prime) {
+				return num;
+			}
+			
+			num++;
+			
+		}
+	}
+}
