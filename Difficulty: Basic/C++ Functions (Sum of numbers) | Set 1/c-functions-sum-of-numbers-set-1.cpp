93769c1bd30c16de9213235624e9422018aaa8cc
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-27 14:24:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ Functions (Sum of numbers) | Set 1/c-functions-sum-of-numbers-set-1.cpp
@@ -0,0 +1,12 @@
+/* Write your function here */
+
+/* The function should be named calcSum
+and accepts three parameters of integer type
+and return s the sum of the three integers */
+
+int calcSum(int a, int b, int c) {
+	// Your code here
+	int s = a + b + c;
+	
+	return s;
+}
