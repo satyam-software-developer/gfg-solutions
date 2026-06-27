2db46174621ddc28e72b3631ae06e468450125c0
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-27 14:41:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find one extra character/find-one-extra-character.java
@@ -0,0 +1,16 @@
+class Solution {
+	public char extraChar(String s1, String s2) {
+		// code here
+		
+		int sum1 = 0, sum2 = 0;
+		
+		for (int i = 0; i < s1.length(); i++) {
+			sum1 += s1.charAt(i);
+		}
+		for (int i = 0; i < s2.length(); i++) {
+			sum2 += s2.charAt(i);
+		}
+		
+		return (char) (sum2 - sum1);
+	}
+}
