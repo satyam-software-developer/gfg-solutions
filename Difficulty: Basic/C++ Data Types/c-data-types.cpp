6daf2934e19188de73c4b2603a757b20ab7a183c
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-02 14:28:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ Data Types/c-data-types.cpp
@@ -0,0 +1,25 @@
+class Solution {
+	public:
+	int cppIntType() {
+		// code here
+		int x;
+		cin>> x;
+		return x;
+		
+	}
+	
+	char cppCharType() {
+		
+		// code here
+		char ch;
+		cin>> ch;
+		return ch;
+	}
+	
+	float cppFloatType() {
+		// code here
+		float f;
+		cin>> f;
+		return f;
+	}
+};
