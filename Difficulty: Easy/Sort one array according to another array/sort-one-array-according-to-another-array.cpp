f04aa82de70c24fc0beab7508808e1537118030e
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-08 14:36:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort one array according to another array/sort-one-array-according-to-another-array.cpp
@@ -0,0 +1,16 @@
+vector<char> sortArray(vector<int>& a, vector<char>& b) {
+	vector<pair<int, char>> v;
+	
+	for (int i = 0; i < a.size(); i++) {
+		v.push_back({a[i], b[i]});
+	}
+	
+	sort(v.begin(), v.end());
+	
+	vector<char> ans;
+	for (int i = 0; i < v.size(); i++) {
+		ans.push_back(v[i].second);
+	}
+	
+	return ans;
+}
